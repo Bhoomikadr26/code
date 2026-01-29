c2b52cebae1ef12b4168b465b34b1903bf5579dc
Bhoomikadr26 <bhoomikadr26@gmail.com>
2026-01-29 21:24:10 +0530

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,13 @@
+class Bank {
+    public static void main(String[] args) {
+
+        int balance = 1000;
+        int deposit = 500;
+        int withdraw = 300;
+
+        balance = balance + deposit;
+        balance = balance - withdraw;
+
+        System.out.println("Final Balance = " + balance);
+    }
+}
